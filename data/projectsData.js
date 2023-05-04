1857071a1cd619a2ea07fb98fa457b8513592410
Shouryan Nikam <snnikam@umich.edu>
2023-05-04 14:09:57 -0400

    create post for blazor template

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,13 +2,24 @@ const projectsData = [
   {
     title: 'OpenCV Rock-Paper-Scissors',
     description: `When you really love playing rock-paper-scissors, why not make a game that can run directly in the browser (or locally as a .py script)!`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/paper-rock-scissors.jpg',
     href: '/blog/opencv-rock-paper-scissors',
     github: 'https://github.com/shouryan01/opencv-rock-paper-scissors',
     tech1: 'Python',
     tech2: 'OpenCV',
     tech3: 'MediaPipe',
     tech4: 'HTML',
+  },
+  {
+    title: 'Blazor Starter Template',
+    description: `Microsoft's implementation of a intro to blazor isn't very good. So I made my own!`,
+    imgSrc: '/static/images/blazor.png',
+    href: '/blog/blazor-template',
+    github: 'https://github.com/shouryan01/BlazorServerTemplate',
+    tech1: 'C#',
+    tech2: 'HTML',
+    tech3: 'JavaScript',
+    tech4: 'CSS',
   },
 ]
 
