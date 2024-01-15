cb8cbbb2af7e5db7246be5d40190af90100f5116
Shouryan Nikam <dev.shouryan@gmail.com>
2024-01-14 17:49:43 -0800

    add v2 changes replace eslint+prettier with biomejs add bun package manager

---
+++ components/Comments.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Comments as CommentsComponent } from 'pliny/comments'
+import siteMetadata from '@/data/siteMetadata'
+import { useState } from 'react'
+
+export default function Comments({ slug }: { slug: string }) {
+  const [loadComments, setLoadComments] = useState(false)
+  return (
+    <>
+      {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
+      {siteMetadata.comments && loadComments && (
+        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+      )}
+    </>
+  )
+}
