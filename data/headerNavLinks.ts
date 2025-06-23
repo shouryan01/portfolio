371bd4d5bcbb8a367d175fc8e7c3365c560b725d
Shouryan Nikam <dev.shouryan@gmail.com>
2025-06-22 22:12:05 -0700

    commit and push more files

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: 'https://photos.shouryannikam.com', title: 'Photos' },
 ]
 
 export default headerNavLinks
